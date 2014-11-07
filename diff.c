2299ec2bf50f481e67e3fe04099bbe5caf4a478a
David Honour <david@foolswood.co.uk>
2014-11-07 17:30:17 +0000

    Assemble contiguous blocks from unique chunks

--- diff.c
+++ diff.c
@@ -1,32 +1,17 @@
 #include <glib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef char* buffer;
 
 typedef unsigned long hash;
 
-typedef struct view {
+typedef struct chunk {
     buffer source;  // raw data
     unsigned start;  // start index in raw data
     unsigned end;  // end index in raw data
     buffer data;  // source + start
     unsigned length;  // end - start
-} view;
-
-view view_new(const buffer source, const unsigned start, const unsigned end) {
-    return (view) {
-        .source = source,
-        .start = start,
-        .end = end,
-        .data = source + start,
-        .length = end - start};
-}
-
-typedef struct chunk {
-//    const union {
-//        const struct view;
-        view v;
-//    };
     hash hash;
 } chunk;
 
@@ -35,7 +20,11 @@ chunk chunk_new(const buffer source, const unsigned start, const unsigned end) {
     snprintf(substr, end - start + 1, "%s", source + start);
     //printf("substr: %u %u %s\n", start, end, substr);
     return (chunk) {
-        .v = view_new(source, start, end),
+        .source = source,
+        .start = start,
+        .end = end,
+        .data = source + start,
+        .length = end - start,
         .hash = g_str_hash(substr)};
 }
 
@@ -63,24 +52,61 @@ void hash_set_destroy(hash_set set) {
     g_hash_table_destroy(set);
 }
 
-chunks unique_blocks(restrict chunks our_chunks, restrict chunks others_chunks) {
+typedef chunk block;
+// The hash of a block is the hash of its preceeding chunk common to both
+// streams used as an anchor to align the streams.
+
+
+// Watch out mallocy
+block * block_new(
+        const buffer source, chunk const * const previous_common,
+        const unsigned end) {
+    const unsigned start = previous_common->end;
+    block * const new_block = malloc(sizeof(block));
+    *new_block = (block) {
+        .source = source,
+        .start = start,
+        .end = end,
+        .data = source + start,
+        .length = end - start,
+        .hash = previous_common->hash};
+    return new_block;
+}
+
+typedef GSList * blocks;
+
+blocks unique_blocks(restrict chunks ours, restrict chunks theirs) {
     typedef chunk const * const const_chunk;
-    hash some_hash = ((const_chunk) others_chunks->data)->hash;
-    hash_set others_hashes = hash_set_new();
-    while (others_chunks != NULL) {
-        hash_set_insert(others_hashes, ((const_chunk) others_chunks->data)->hash);
-        others_chunks = g_slist_next(others_chunks);
+    hash_set their_hashes = hash_set_new();
+    while (theirs != NULL) {
+        hash_set_insert(their_hashes, ((const_chunk) theirs->data)->hash);
+        theirs = g_slist_next(theirs);
     }
 
-    chunks unique_chunks = NULL;
-    while (our_chunks != NULL) {
-        if (!hash_set_contains(others_hashes, ((const_chunk) our_chunks->data)->hash)) {
-            unique_chunks = g_slist_prepend(unique_chunks, our_chunks->data);
+    blocks unique_blocks = NULL;
+    chunk zero = chunk_new(NULL, 0, 0);
+    chunk const * previous_common = &zero;
+    chunk const * chunk;
+    while (ours != NULL) {
+        chunk = ours->data;
+        if (hash_set_contains(their_hashes, chunk->hash)) {
+            // We're processing a chunk common to ours and theirs
+            if (chunk->start != previous_common->end) {
+                // There's a gap, we skipped over some chunks unique to ours
+                unique_blocks = g_slist_prepend(unique_blocks, block_new(
+                    chunk->source, previous_common, chunk->start));
+            }
+            previous_common = chunk;
         }
-        our_chunks = g_slist_next(our_chunks);
+        ours = g_slist_next(ours);
     }
-    hash_set_destroy(others_hashes);
-    return g_slist_reverse(unique_chunks);
+    if (chunk->start != previous_common->start) {
+        // Unique block at end
+        unique_blocks = g_slist_prepend(unique_blocks, block_new(
+            chunk->source, previous_common, chunk->start));
+    }
+    hash_set_destroy(their_hashes);
+    return g_slist_reverse(unique_blocks);
 }
 
 int main() {
@@ -100,10 +126,12 @@ int main() {
     b_chunks = g_slist_prepend(b_chunks, &b0);
 
     chunks unique = unique_blocks(b_chunks, a_chunks);
-    printf("unique bit: %s\n", ((chunk *) unique->data)->v.data);
+    if (unique != NULL) {
+        chunk * c = unique->data;
+        printf("unique bit: %s\n", c->data);
+    } else
+        printf("no unique stuff\n");
 }
-
-typedef struct view block;
 
 typedef struct {
     unsigned start;
@@ -112,9 +140,6 @@ typedef struct {
 } diff_hunk;
 
 typedef diff_hunk* diff;
-
-diff diff_new(const view a, const view b) {
-}
 
 /*
 diff file_diff(const string a, const string b) {
