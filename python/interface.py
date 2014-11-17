248c41005f288e9e509c9822225723aba63e7e89
Paul Weaver <paul@ruthorn.co.uk>
2014-11-17 23:59:28 +0000

    Make rudimentary keybindings, handle quit

--- python/interface.py
+++ python/interface.py
@@ -1,4 +1,5 @@
 from asyncio import get_event_loop
+import sys
 
 from terminal import Terminal, Keyboard
 
@@ -9,17 +10,28 @@ class DiffApp:
     def __init__(self):
         self.terminal = Terminal()
         self.keyboard = Keyboard()
+        self.bindings = {
+            'q': lambda _: self._do_exit('Quit normally')}
         self.loop = get_event_loop()
 
+    @staticmethod
+    def _do_exit(message):
+        sys.exit(message)
+
     def _read_stdin(self):
-        print(self.keyboard[self.terminal.infile.read()])
+        key = self.keyboard[self.terminal.infile.read()]
+        action = self.bindings.get(key, lambda _: None)
+        action(key)
 
     def run(self):
         if self.terminal.infile.isatty():
             self.loop.add_reader(self.terminal.infile, self._read_stdin)
         with self.terminal.unbuffered_input(), (
                 self.terminal.nonblocking_input()):
-            self.loop.run_forever()
+            try:
+                self.loop.run_forever()
+            except KeyboardInterrupt:
+                self._do_exit('Keyboard interrupt')
 
 
 if __name__ == '__main__':
