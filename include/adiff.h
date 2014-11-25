c1266d1839f8ad7b61c3cdacb8ebf930acb69eb4
David Honour <david@foolswood.co.uk>
2014-11-25 07:54:29 +0000

    Extend adiff interface to leave space for error handling

--- include/adiff.h
+++ include/adiff.h
@@ -2,4 +2,20 @@
 
 #include "diff_types.h"
 
-hunk * adiff(char const * const a_path, char const * const b_path);
+enum adiff_return_code {
+    ADIFF_OK = 0,
+    ADIFF_ERR_CHANNELS,
+    ADIFF_ERR_SAMPLE_RATE,
+    ADIFF_ERR_SAMPLE_FORMAT,
+    ADIFF_ERR_OPEN_A,
+    ADIFF_ERR_OPEN_B
+};
+
+typedef struct {
+    adiff_return_code code;
+    hunk * hunks;
+} diff;
+
+diff adiff(char const * const a_path, char const * const b_path);
+
+void diff_free(diff d);
