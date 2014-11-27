7cec4e880bc3a599019d78e75a5afda3b4ccebcf
Paul Weaver <paul@ruthorn.co.uk>
2014-11-27 02:09:21 +0000

    Document the purpose of blocks and the unique_blocks function

--- src/block.h
+++ src/block.h
@@ -1,8 +1,19 @@
 #pragma once
 #include "chunk.h"
 
-// The hash of a block is the hash of its preceeding chunk common to both
-// streams used as an anchor to align the streams.
+/*! A block data is very similar to a chunk of data in our abstraciton. That
+ * is, it is a simple view onto the data with an additional hash property.
+ *
+ * Conceptually, our blocks are formed from multiple chunks of data that are
+ * unique to only one of the data streams we are comparing - they are "bigger"
+ * units.
+ *
+ * Unlike chunks, the hash of a block is not the hash of the contents,
+ * but rather the hash of the preceeding "achor" chunk that is common to both
+ * of the streams being compared. This hash is used as a reference point so we
+ * can align the streams and compare which set of unique data in one stream can
+ * be rightly compared to which in the other.
+ */
 typedef chunk block;
 
 typedef block * blocks;
