90af5e14a7d150650d517920c07fbe345ef1e2e3
Paul Weaver <paul@ruthorn.co.uk>
2014-11-27 10:20:29 +0000

    Fix typos in chunk/block docs

--- src/block.h
+++ src/block.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "chunk.h"
 
-/*! A block data is very similar to a chunk of data in our abstraciton. That
+/*! A block data is very similar to a chunk of data in our abstraction. That
  * is, it is a simple view onto the data with an additional hash property.
  *
  * Conceptually, our blocks are formed from multiple chunks of data that are
@@ -9,7 +9,7 @@
  * units.
  *
  * Unlike chunks, the hash of a block is not the hash of the contents,
- * but rather the hash of the preceeding "achor" chunk that is common to both
+ * but rather the hash of the preceeding "anchor" chunk that is common to both
  * of the streams being compared. This hash is used as a reference point so we
  * can align the streams and compare which set of unique data in one stream can
  * be rightly compared to which in the other.
--- src/chunk.c
+++ src/chunk.c
@@ -21,7 +21,7 @@ static const unsigned buf_size = 4096;
  * We read data into a buffer using the provided data_fetcher, then we use a
  * rolling hash over that data to split based on the content (we split when the
  * lower n bits of the hash is zero). That means that our splitting is
- * resistant to shifts in the data from inserions and deletions. which is very
+ * resistant to shifts in the data from insertions and deletions, which is very
  * useful for comparison with other data.
  */
 chunks const split_data(data_fetcher const df, void * const source) {
