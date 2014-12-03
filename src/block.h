dd027a860b57739982170f452c5de705a77dcdd3
Paul Weaver <paul@ruthorn.co.uk>
2014-12-03 11:19:36 +0000

    Update block docs

--- src/block.h
+++ src/block.h
@@ -1,18 +1,14 @@
 #pragma once
 #include "chunk.h"
 
-/*! A block data is very similar to a chunk of data in our abstraction. That
- * is, it is a simple view onto the data with an additional hash property.
+/*! A block of data is very similar to a chunk of data in our abstraction. That
+ * is, it is a simple view onto the data and a small amount of metadata.
  *
- * Conceptually, our blocks are formed from multiple chunks of data that are
+ * Conceptually our blocks are formed from multiple chunks of data that are
  * unique to only one of the data streams we are comparing - they are "bigger"
- * units.
- *
- * Unlike chunks, the hash of a block is not the hash of the contents,
- * but rather the hash of the preceeding "anchor" chunk that is common to both
- * of the streams being compared. This hash is used as a reference point so we
- * can align the streams and compare which set of unique data in one stream can
- * be rightly compared to which in the other.
+ * units. Instead of a hash, the metadata we store is used as a reference to
+ * tie the position of this unique data to a position in the comparison stream
+ * of data.
  */
 typedef struct block {
     union {
