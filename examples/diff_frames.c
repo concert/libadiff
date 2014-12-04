2c03c21aff5f5db5cdfe3652a66c541b749568e2
Paul Weaver <paul@ruthorn.co.uk>
2014-12-04 23:01:24 +0000

    Output user-facing messages in diff_frames to stderr

--- examples/diff_frames.c
+++ examples/diff_frames.c
@@ -3,31 +3,31 @@
 
 int main(int argc, char ** argv) {
     if (argc != 3) {
-        printf("Usage: diff_frames file_a file_b\n");
+        fprintf(stderr, "Usage: diff_frames file_a file_b\n");
         return -1;
     }
     diff d = adiff(argv[1], argv[2]);
     switch (d.code) {
         case ADIFF_ERR_OPEN_A:
-            printf("Failed to open: %s\n", argv[1]);
+            fprintf(stderr, "Failed to open: %s\n", argv[1]);
             break;
         case ADIFF_ERR_OPEN_B:
-            printf("Failed to open: %s\n", argv[2]);
+            fprintf(stderr, "Failed to open: %s\n", argv[2]);
             break;
         case ADIFF_ERR_CHANNELS:
-            printf("Files have different numbers of channels\n");
+            fprintf(stderr, "Files have different numbers of channels\n");
             break;
         case ADIFF_ERR_SAMPLE_RATE:
-            printf("Files have different sample rates\n");
+            fprintf(stderr, "Files have different sample rates\n");
             break;
         case ADIFF_ERR_SAMPLE_FORMAT:
-            printf("Files have different sample formats\n");
+            fprintf(stderr, "Files have different sample formats\n");
             break;
         case ADIFF_OK:
             if (d.hunks == NULL) {
-                printf("No changes found");
+                fprintf(stderr, "No changes found");
             } else {
-                printf("We have a diff");
+                fprintf(stderr, "OK");
             }
     }
     diff_free(&d);
