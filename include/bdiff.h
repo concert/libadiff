9201e9cf2d1c09294fd4ff4e0947587fb95e4c12
David Honour <david@foolswood.co.uk>
2014-12-16 07:46:04 +0000

    Add some more docs.

--- include/bdiff.h
+++ include/bdiff.h
@@ -2,9 +2,20 @@
 
 #include "diff_types.h"
 
+/**
+ * A function to be supplied by the library user for getting the data to diff.
+ */
 typedef unsigned (*data_fetcher)(
     void * source, unsigned n_items, char * buffer);
 
+/** \brief Perform a binary diff.
+ *
+ * \param[in] sample_size The size (in bytes) of an item in the data type being
+ * diffed.
+ * \param[in] df Function to use to get the data from a and b.
+ * \param[in] a Given as the source parameter to df for the a side.
+ * \param[in] b Given as the source parameter to df for the b side.
+ */
 hunk * const bdiff(
     unsigned const sample_size, data_fetcher const df, void * const a,
     void * const b);
--- include/rabin.h
+++ include/rabin.h
@@ -5,19 +5,30 @@
 typedef uint32_t hash;
 
 
+/** \brief Opaque structure holding state for hashing.
+ */
 typedef struct hash_data {
     hash h;
     hash irreducible_polynomial;
     hash table[256];
 } hash_data;
 
+/** \brief Initialise the hashing state with the given irreducible polynomial.
+ */
 hash_data hash_data_init(hash const irreducible_polynomial);
 
+/** \brief Return the hash to a state where no data has been hashed.
+ */
 void hash_data_reset(hash_data * const hd);
 
+/** \brief Hash a byte of data.
+ * \return The new hash.
+ */
 hash hash_data_update(hash_data * const hd, unsigned char const next);
 
 
+/** \brief Opaque structure holding state for windowed hashing.
+ */
 typedef struct {
     union {
         struct hash_data;
@@ -29,10 +40,22 @@ typedef struct {
     hash undo_table[256];
 } window_data;
 
+/** \brief Initialise the windowed hashing state.
+ * \param[in] hd Hash data structure to use as the basis for the
+ * windowed hash (i.e. to use for its irreducible polynomial).
+ * \param[in] window_buffer Buffer (at least window_size bytes in size) to use
+ * for storing the data currently in the window.
+ * \param[in] window_size Number of bytes in the window.
+ */
 window_data window_data_init(
     hash_data const * const hd, unsigned char * const window_buffer,
     unsigned const window_size);
 
+/** \brief Return the windowed hash to a state where no data has been hashed.
+ */
 void window_data_reset(window_data * const wd);
 
+/** \brief Hash a byte of data.
+ * \return The hash of the new window.
+ */
 hash window_data_update(window_data * const wd, unsigned char const next);
