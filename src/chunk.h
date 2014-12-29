4eea735d087ef743dc1d0ef5178938371918ee53
David Honour <david@foolswood.co.uk>
2014-12-29 11:01:55 +0000

    QT style to javadoc style for start of doxygen comments.

--- src/chunk.h
+++ src/chunk.h
@@ -2,7 +2,7 @@
 #include "../include/bdiff.h"
 #include "hash.h"
 
-/*! \brief Shift resistant block.
+/** \brief Shift resistant block.
  *
  * Linked list of views with hashes.
  */
@@ -17,7 +17,7 @@ typedef struct chunk {
 
 typedef chunk * chunks;
 
-/*! \brief Create a new chunk on the heap.
+/** \brief Create a new chunk on the heap.
  *
  * \param[out] prev pointer to the chunk to append the new one to.
  * \param[in] start the starting index of the chunk.
@@ -29,7 +29,7 @@ chunk * chunk_new(
         chunk * const prev, unsigned const start, unsigned const end,
         hash const h);
 
-/*! \brief Split the data given by the data_fetcher into shift resistant blocks.
+/** \brief Split the data given by the data_fetcher into shift resistant blocks.
  *
  * \param[in] sample_size the size of a sample returned by the data fetcher.
  * \param[in] df a data_fetcher function.
@@ -39,7 +39,7 @@ chunk * chunk_new(
 chunks const split_data(
     unsigned const sample_size, data_fetcher const df, void * const source);
 
-/*! \brief Free a linked list of chunks.
+/** \brief Free a linked list of chunks.
  *
  * \param[out] head pointer to the start of the list.
  */
