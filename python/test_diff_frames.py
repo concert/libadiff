fb61a98d22afb568a132b92c4cf5bf97d4fc6476
Paul Weaver <paul@ruthorn.co.uk>
2015-01-06 23:43:54 +0000

    Add test for PySndfile duration calc

--- python/test_diff_frames.py
+++ python/test_diff_frames.py
@@ -1,8 +1,17 @@
 from unittest import TestCase
-from diff_frames import fmt_seconds
+from mock import Mock
+
+from pysndfile import PySndfile
+from diff_frames import fmt_seconds, duration
 
 
 class TestDiffFrames(TestCase):
+    def test_duration(self):
+        mock_snd_file = Mock(spec=PySndfile)
+        mock_snd_file.frames.return_value = 44100 * 42 + 441
+        mock_snd_file.samplerate.return_value = 44100
+        self.assertEqual(duration(mock_snd_file), 42.01)
+
     def test_fmt_seconds(self):
         self.assertEqual(fmt_seconds(1.567), '1.57')
         self.assertEqual(fmt_seconds(59.899), '59.90')
