3795f988be237f967b0e2071c83bc8c4d3f1af0e
David Honour <david@foolswood.co.uk>
2015-01-18 10:54:18 +0000

    Adjust the limits to something slightly more appropriate.

--- src/bdiff.c
+++ src/bdiff.c
@@ -6,8 +6,8 @@
 hunk * const bdiff(
         unsigned const sample_size, data_fetcher const df, void * const a,
         void * const b) {
-    chunks a_chunks = split_data(sample_size, df, a, 1, 100000);
-    chunks b_chunks = split_data(sample_size, df, b, 1, 100000);
+    chunks a_chunks = split_data(sample_size, df, a, 10, 10000);
+    chunks b_chunks = split_data(sample_size, df, b, 10, 10000);
     hunk * const h = diff_chunks(a_chunks, b_chunks);
     chunk_free(a_chunks);
     chunk_free(b_chunks);
